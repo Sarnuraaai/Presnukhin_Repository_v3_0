c6eea329e57ee1b42a68552c6036934df13dfd56
Sarnuraaai <149012432+Sarnuraaai@users.noreply.github.com>
2023-12-10 16:44:14 +0300

    Update Master-ветка.cpp

--- Presnukhin_Repository_v3_0/Master-ветка(решение)/Master-ветка/Master-ветка.cpp
+++ Presnukhin_Repository_v3_0/Master-ветка(решение)/Master-ветка/Master-ветка.cpp
@@ -1,19 +1,36 @@
-﻿
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
+
 
 float EnterNumberX(float x)
 {
-    cout << endl << "Введите вещественное число х" << endl << endl;
-    cin >> x;
-    return x;
+        cout << endl << "Введите вещественное число x" << endl << endl;
+        cin >> x;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "Неправильный ввод!" << endl;
+            return 0;
+        }
+        cout << "Успешный ввод!" << endl;
+        return x;
 }
 
 float EnterNumberY(float y)
 {
     cout << endl << "Введите вещественное число y" << endl << endl;
     cin >> y;
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(32767, '\n');
+        cout << "Неправильный ввод!" << endl;
+        return 0;
+    }
+    cout << "Успешный ввод!" << endl;
     return y;
 }
 
@@ -37,6 +54,7 @@ int main()
 {
     setlocale(LC_ALL, "RU");
     int n = 9;
+    string s;
     float x = 0, y = 0;
     while (n != 0)
     {
@@ -45,7 +63,21 @@ int main()
         cout << "3. Найти целую часть разности этих двух чисел\n";
         cout << "4. Найти дробную часть разности этих двух чисел\n" << endl;
         cout << "Введите номер желаемой функции или 0, если хотите завершить программу" << endl << endl;
-        cin >> n;
+        cin >> s;
+        if (s.length() > 1 || ((s[0] != '1') && (s[0] != '2') && (s[0] != '3') && (s[0] != '4') && (s[0] != '0')))
+        {
+            do
+            {
+                cin.clear();
+                cin.ignore(32767, '\n');
+                cout << "Неправильный ввод!" << endl;
+                cout << "Введите номер желаемой функции или 0, если хотите завершить программу" << endl;
+                cin >> s;
+            } while (s.length() > 1 && (s[0] != '1') && (s[0] != '2') && (s[0] != '3') && (s[0] != '4') && (s[0] != '0'));
+        }
+        else
+            n = stof(s);
+
         switch (n)
         {
         case 1:
@@ -65,3 +97,4 @@ int main()
     cout << "Программа завершена.";
     return 0;
 }
+
