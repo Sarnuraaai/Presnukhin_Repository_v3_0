1c76316eab82609cf71c3c5cb60ec20c3da68263
Андрей Преснухин <andrey.presnukhin@gmail.com>
2023-12-24 14:08:42 +0300

    Проект с тестами

---
+++ Presnukhin_Testing_VS/Presnukhin_Test/Presnukhin_Test.cpp
@@ -0,0 +1,81 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Presnukhin_Testing_VS/Presnukhin_Testing_VS.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PresnukhinTest
+{
+	TEST_CLASS(PresnukhinTest)
+	{
+	public:
+		TEST_METHOD(check_UserInput_EmptyValue)
+		{
+			string str = "";
+			bool expected = false;
+
+			bool actual = UserInput(str);
+
+			Assert::AreEqual(expected, actual, L"Entered empty value");
+		}
+		TEST_METHOD(Check_UserInput_Letter_and_Int)
+		{
+			string str = "a2";
+			bool expected = false;
+
+			bool actual = UserInput(str);
+
+			Assert::AreEqual(expected, actual, L"Entered empty value");
+		}
+		TEST_METHOD(Check_UserInput_Letter)
+		{
+			string str = "a";
+			bool expected = false;
+
+			bool actual = UserInput(str);
+
+			Assert::AreEqual(expected, actual, L"Entered empty value");
+		}
+		TEST_METHOD(CalcFloat_771723and55444)
+		{
+			float a = 77.723222;
+			float b = 55.544111;
+			float expected = 0.179111;
+
+			float actual = CalcFloat(a, b);
+
+			Assert::AreEqual(expected, actual, L"Incorrect value area");
+		}
+
+		TEST_METHOD(CalcFloat_3123and5444)
+		{
+			float a = 3.123333;
+			float b = 5.544444;
+			float expected = 0.421111;
+
+			float actual = CalcFloat(a, b);
+
+			Assert::AreEqual(expected, actual, L"Incorrect value area");
+		}
+		TEST_METHOD(CalcInt_77123and55444)
+		{
+			float a = 77.123;
+			float b = 55.544;
+			int expected = 21;
+
+			int actual = CalcInt(a, b);
+
+			Assert::AreEqual(expected, actual, L"Incorrect value area");
+		}
+		TEST_METHOD(CalcInt_3123and5444)
+		{
+			float a = 3.123;
+			float b = 5.544;
+			int expected = 2;
+
+			int actual = CalcInt(a, b);
+
+			Assert::AreEqual(expected, actual, L"Incorrect value area");
+		}
+	};
+}
---
+++ Presnukhin_Testing_VS/Presnukhin_Testing_VS/Presnukhin_Testing_VS.cpp
@@ -0,0 +1,137 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+bool UserInput(string input)
+{
+	if (input.empty())
+	{
+		//cin.clear();
+		//cin.ignore(32767, '\n');
+		return false;
+	}
+	try
+	{
+		float number = stof(input);
+	}
+	catch (...)
+	{
+		//cin.clear();
+		//cin.ignore(32767, '\n');
+		return false;
+	}
+
+	return true;
+}
+
+float EnterNumberX()
+{
+	string str_x;
+	cout << "Введите вещественное число x" << endl;
+	cin >> str_x;
+	while (!UserInput(str_x))
+	{
+		cout << "Неправильный ввод!" << endl;
+		cout << "Введите вещественное число x" << endl << endl;
+		cin >> str_x;
+	}
+	cout << "Успешный ввод!" << endl;
+	return stof(str_x);
+}
+
+float EnterNumberY()
+{
+	string str_y;
+	cout << "Введите вещественное число y" << endl;
+	cin >> str_y;
+	while (!UserInput(str_y))
+	{
+		cout << "Неправильный ввод!" << endl;
+		cout << "Введите вещественное число y" << endl << endl;
+		cin >> str_y;
+	}
+	cout << "Успешный ввод!" << endl;
+	return stof(str_y);
+}
+/*
+float EnterNumberY(float y)
+{
+	cout << endl << "Введите вещественное число y" << endl << endl;
+	cin >> y;
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(32767, '\n');
+		cout << "Неправильный ввод!" << endl;
+		return 0;
+	}
+	cout << "Успешный ввод!" << endl;
+	return y;
+}
+*/
+
+int CalcInt(float x, float y)
+{
+	float func = 0, integer = 0;
+	func = modf(abs(x - y), &integer);
+	cout << endl << "Целая часть разности вещественных чисел равна = " << integer << endl << endl;
+	return integer;
+}
+
+float CalcFloat(float x, float y)
+{
+	cout << x << endl << y << endl;
+	float func = 0, integer = 0;
+	func = modf(abs(x - y), &integer);
+	cout << endl << "Дробная часть разности вещественных чисел равна = " << func << endl << endl;
+	return func;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int n = 9;
+	string s;
+	float y = 0;
+	float x = 0;
+	while (n != 0)
+	{
+		cout << endl << "1. Введите вещественное число х\n";
+		cout << "2. Введите вещественное число y\n";
+		cout << "3. Найти целую часть разности этих двух чисел\n";
+		cout << "4. Найти дробную часть разности этих двух чисел\n" << endl;
+		cout << "Введите номер желаемой функции или 0, если хотите завершить программу" << endl << endl;
+		cin >> s;
+		if (s.length() > 1 || ((s[0] != '1') && (s[0] != '2') && (s[0] != '3') && (s[0] != '4') && (s[0] != '0')))
+		{
+			do
+			{
+				cout << "Неправильный ввод!" << endl;
+				cout << "Введите номер желаемой функции или 0, если хотите завершить программу" << endl;
+				cin >> s;
+			} while (s.length() > 1 && (s[0] != '1') && (s[0] != '2') && (s[0] != '3') && (s[0] != '4') && (s[0] != '0'));
+		}
+		else
+			n = stoi(s);
+
+		switch (n)
+		{
+		case 1:
+			x = EnterNumberX();
+			break;
+		case 2:
+			y = EnterNumberY();
+			break;
+		case 3:
+			CalcInt(x, y);
+			break;
+		case 4:
+			CalcFloat(x, y);
+			break;
+		}
+	}
+	cout << "Программа завершена.";
+	return 0;
+}
+
